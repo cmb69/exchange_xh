ddf3c7b6cb985a6854e39f860f62fa87a518d139
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-21 14:53:21 +0200

    Add French localization

---
+++ languages/fr.php
@@ -0,0 +1,24 @@
+<?php
+
+$plugin_tx['exchange']['menu_main']="Importer / Exporter";
+$plugin_tx['exchange']['alt_logo']="Flèches tournantes";
+$plugin_tx['exchange']['label_export']="Exporter";
+$plugin_tx['exchange']['label_import']="Importer";
+$plugin_tx['exchange']['message_export_overwrite']="Le fichier existant \"content.xml\" sera écrasé à l'exportation !";
+$plugin_tx['exchange']['message_exported']="L'actuel fichier de contenu a été exporté (format XML).";
+$plugin_tx['exchange']['message_export_failed']="Échec de l'exportation du contenu !";
+$plugin_tx['exchange']['message_import_overwrite']="Le contenu actuel sera écrasé à l'importation du nouveau fichier !";
+$plugin_tx['exchange']['message_no_import']="\"content.xml\" n'a pas été trouvé, il n'y a rien à importer.";
+$plugin_tx['exchange']['message_imported']="Le contenu a été importé (format HTML).";
+$plugin_tx['exchange']['message_import_failed']="Échec de l'importation du contenu !";
+$plugin_tx['exchange']['syscheck_extension']="L'extension PHP '%s' est chargée";
+$plugin_tx['exchange']['syscheck_fail']="Erreur";
+$plugin_tx['exchange']['syscheck_message']="Vérification : %1\$s … %2\$s";
+$plugin_tx['exchange']['syscheck_phpversion']="Version PHP ≥ %s";
+$plugin_tx['exchange']['syscheck_success']="Ok";
+$plugin_tx['exchange']['syscheck_title']="Vérification du système";
+$plugin_tx['exchange']['syscheck_warning']="avertissement";
+$plugin_tx['exchange']['syscheck_writable']="'%s' peut-être écrit";
+$plugin_tx['exchange']['syscheck_xhversion']="Version CMSimple_XH ≥ %s";
+
+?>
