2125aa1aa8577df0329100ac14f4aedd7b94a2d3
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-13 19:48:20 +0200

    Use fieldsets in import/export form
    That's much more sensible than just a `<hr>`.

--- views/main.php
+++ views/main.php
@@ -16,21 +16,26 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
 <section class="exchange_main">
   <h1><?=$this->text('menu_main')?></h1>
   <form method="post">
-    <input type="hidden" name="exchange_token" value="<?=$this->esc($csrfToken)?>">
+    <fieldset>
+      <legend><?=$this->text('label_export')?></legend>
+      <input type="hidden" name="exchange_token" value="<?=$this->esc($csrfToken)?>">
 <?if ($hasXmlFile):?>
-    <p class="xh_warning"><?=$this->text('message_export_overwrite')?></p>
+      <p class="xh_warning"><?=$this->text('message_export_overwrite')?></p>
 <?endif?>
-    <p>
-      <button formaction="<?=$this->esc($export_url)?>"><?=$this->text('label_export')?></button>
-    </p>
-    <hr>
+      <p>
+        <button formaction="<?=$this->esc($export_url)?>"><?=$this->text('label_export')?></button>
+      </p>
+    </fieldset>
+    <fieldset>
+      <legend><?=$this->text('label_import')?></legend>
 <?if ($hasXmlFile):?>
-    <p class="xh_warning"><?=$this->text('message_import_overwrite')?></p>
-    <p>
-      <button formaction="<?=$this->esc($import_url)?>"><?=$this->text('label_import')?></button>
-    </p>
+      <p class="xh_warning"><?=$this->text('message_import_overwrite')?></p>
+      <p>
+        <button formaction="<?=$this->esc($import_url)?>"><?=$this->text('label_import')?></button>
+      </p>
 <?else:?>
-    <p class="xh_info"><?=$this->text('message_no_import')?></p>
+      <p class="xh_info"><?=$this->text('message_no_import')?></p>
 <?endif?>
+    </fieldset>
   </form>
 </section>
